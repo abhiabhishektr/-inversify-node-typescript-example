0204a8c30844f739240e5163f423fcc86897e69d
abhishek <abhiabhishektr@gmail.com>
2024-09-25 17:40:10 +0530

    unwanted files deleted and added gitignore

--- dist/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// src/app.ts
-require("reflect-metadata");
-const inversify_config_1 = require("./inversify.config");
-const logger = inversify_config_1.container.get("ILogger");
-logger.log("Inversify and TypeScript setup complete!");
